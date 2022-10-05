9944b1af9b57bf70df12a5b8570b1b07ac3774ec
wilson-castro <wilsoncastro.pv@gmail.com>
2022-10-05 15:58:50 -0300

    Criação das rotas de produtos  e adição de validação nas rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import productRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
+
+routes.use('/products', productRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
